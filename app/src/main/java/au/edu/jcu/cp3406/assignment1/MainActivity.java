0bc26f81d6c3035597fe7fbe2b0615ee721d1ee9
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2021-03-20 09:44:39 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/assignment1/MainActivity.java
@@ -0,0 +1,28 @@
+package au.edu.jcu.cp3406.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
