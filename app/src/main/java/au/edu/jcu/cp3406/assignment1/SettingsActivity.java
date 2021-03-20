2b19dad2fdfd4ee91fdf7b5ad35d0c41e1ae1292
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2021-03-20 14:25:05 +1000

    Add layout of settings activity

--- app/src/main/java/au/edu/jcu/cp3406/assignment1/SettingsActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/assignment1/SettingsActivity.java
@@ -1,4 +1,31 @@
 package au.edu.jcu.cp3406.assignment1;
 
-public class SettingsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+
+    public void doneClicked(View view) {
+        EditText userInput = findViewById(R.id.userInput);
+        String text = userInput.getText().toString();
+        int speed = Integer.parseInt(text);
+
+        Intent intent = new Intent();
+        intent.putExtra("speed", speed);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
