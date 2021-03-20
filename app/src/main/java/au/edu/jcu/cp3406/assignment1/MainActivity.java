ab0ef43356141c97dfe9df95cd916ece318539dd
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2021-03-20 14:10:34 +1000

    Create Toolbar and setup files

--- app/src/main/java/au/edu/jcu/cp3406/assignment1/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/assignment1/MainActivity.java
@@ -1,10 +1,13 @@
 package au.edu.jcu.cp3406.assignment1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main_menu, menu);
+        System.out.println("Menu Created");
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Here, a switch case can be made if there are multiple menu items
+        // We only have one, so we can just handle it
+        System.out.println("Settings Pressed");
+
+        Intent settingsIntent = new Intent();
+
+
+
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
